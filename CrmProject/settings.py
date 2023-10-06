ae2aae484bf6f229b76e20d2644f7dab89e30c60
saipa <saipavan861@gmail.com>
2023-10-06 17:37:03 +0530

    INIT

--- CrmProject/settings.py
+++ CrmProject/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-o$+y!h=iz-ul*ihddy$@mx)je$r=-v8mm3v$__267o$9-1^qpt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -123,6 +123,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
+
+
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'assets/static')]
